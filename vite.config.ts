08290f010f684b25c0b9e92073c22b364cc51577
Yu-Shun Hsiao <yu-shunhsiao@MacBook-Pro.local>
2026-01-25 12:47:45 -0500

    base path for custom domain update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Alphora-Group-Landing-Page/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
